34af0b836d58d2c2956e92866db73b232259d1dc
kabuki-starship <kabuki@astartup.net>
2019-12-25 15:44:33 -0800

    Detail.OpenGLTutorials.Textures. #5

---
+++ core.vs
@@ -0,0 +1,15 @@
+#version 330 core
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 color;
+layout (location = 2) in vec2 texCoord;
+
+out vec3 color_1;
+out vec2 tex_coord;
+
+void main()
+{
+    gl_Position = vec4(position, 1.0f);
+    color_1 = color;
+
+    tex_coord = vec2(tex_coord.x, 1.0 - tex_coord.y);
+}
--- environment.h
+++ environment.h
@@ -1,4 +1,4 @@
-/* IGEEK @version 0.x
+/* Interactive Gym Environment and Educaitonal Kit @version 0.x
 @link    https://github.com/kabuki-starship/igeek.git
 @file    /environment.h
 @author  Cale McCollough <https://cale-mccollough.github.io>
@@ -6,3 +6,21 @@
 This Source Code Form is subject to the terms of the Mozilla Public License,
 v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain
 one at <https://mozilla.org/MPL/2.0/>. */
+
+#pragma once
+
+#include <_config.h>
+
+#ifndef IGEEK_SHADER_DECL
+#define IGEEK_SHADER_DECL
+
+namespace _ {
+
+class Environment {
+ public:
+  Environment();
+};
+
+}  // namespace _
+
+#endif  //< IGEEK_SHADER_DECL
